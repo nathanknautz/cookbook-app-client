c8042b55feb62916a17e7512c515d6dee335e061
nathanknautz <nate.knautz@gmail.com>
2018-01-18 21:37:39 -0600

    made it very user friendly

--- runner.rb
+++ runner.rb
@@ -7,6 +7,8 @@ puts "Make a selection"
 puts "    [1] See all recipes"
 puts "    [2] See one recipe"
 puts "    [3] Create a new recipe"
+puts "    [4] Update a recipe"
+puts "    [5] Destroy a recipe"
 
 input_option = gets.chomp
 
@@ -30,10 +32,33 @@ elsif input_option == "3"
   client_params[:ingredients] = gets.chomp
   print "Directions: "
   client_params[:directions] = gets.chomp
-
   response = Unirest.post("http://localhost:3000/recipes",
                           parameters: client_params
                           )
+elsif input_option == "4"
+  print "Enter recipe id: "
+  input_id = gets.chomp
+  response = Unirest.get("http://localhost:3000/recipes/#{input_id}")
+  recipe = response.body
+
+  client_params = {}
+  print "Tile (#{recipe["title"]}): "
+  client_params[:title] = gets.chomp
+  print "Chef (#{recipe["chef"]}): "
+  client_params[:chef] = gets.chomp
+  print "Ingredients (#{recipe["ingredients"]}): "
+  client_params[:ingredients] = gets.chomp
+  print "Directions (#{recipe["directions"]}): "
+  client_params[:directions] = gets.chomp
+  client_params.delete_if {|key, value| value.empty?}
+  response = Unirest.patch("http://localhost:3000/recipes/#{input_id}",
+                          parameters: client_params
+                          )
+elsif input_option == "5"
+  print "Enter recipe id: "
+  input_id = gets.chomp
+  response = Unirest.delete("http://localhost:3000/recipes/#{input_id}")
+  data = response.body
+  puts data["message"]
  end
 
- 
